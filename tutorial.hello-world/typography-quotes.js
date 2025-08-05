214bb3c5de6df357a26a3f86237b255362a320ec
Mandy Chen <mandychen.art@gmail.com>
2025-08-04 19:50:34 -0700

    optical indents per letter

--- tutorial.hello-world/typography-quotes.js
+++ tutorial.hello-world/typography-quotes.js
@@ -1,44 +1,63 @@
-const paragraphs = [...document.querySelectorAll('p')];
-// ~ 108 letters per line /3 = 36  ;  /4 = 27  ; /5 = 21
-let sliceDepth = 30;
-// sliceDepth = 20;
-// removes orphans
-const noBreak = s => s.slice(0, s.length-sliceDepth) + s.slice(-sliceDepth).replace(/ /g, ' ');
-let hang;
+const hangQuotes = () => {
+  const paragraphs = [...document.querySelectorAll('p')];
+  // ~ 108 letters per line /3 = 36  ;  /4 = 27  ; /5 = 21
+  let sliceDepth = 30;
+  // sliceDepth = 20;
+  // removes orphans
+  const noBreak = s => s.slice(0, s.length-sliceDepth) + s.slice(-sliceDepth).replace(/ /g, ' ');
+  let hang;
 
-// get computed font family
-const family = getComputedStyle(paragraphs[0]).getPropertyValue("font-family").split(/, */)[0];
-// set computed font family
-switch (true) {
-  case /.*garamond.*/i.test(family):
-  case /.*BreveText.*/i.test(family):
-    hang = "garamond";
-  case /.*open ?sans.*/i.test(family):
-  case /.*roboto.*/i.test(family):
-    hang = "open";
-  break;
-  case /.*nyt-imperial.*/i.test(family):
-  case /.*indy ?serif.*/i.test(family):
-  case /.*georgia.*/i.test(family):
-    hang = "hand-nyt";
-  break;
-  case /.*merriweather.*/i.test(family):
-    hang = "merriweather";
-  break;
-  case /.*brevetext.*/i.test(family):
-    hang = "brevetext";
-  break;
-  default:
-    hang = "open";
-    console.warn("No matching font family found, using default 'open' hang value.");
+  // get computed font family
+  const mid = Math.floor(paragraphs.length / 2);
+  const family = getComputedStyle(paragraphs[mid-1])?.getPropertyValue("font-family")?.split(/, */)[0];
+  console.log("Computed font family:", family);
+  // set computed font family
+  switch (true) {
+    case /.*garamond.*/i.test(family):
+    case /.*BreveText.*/i.test(family):
+      hang = "garamond";
+    case /.*open ?sans.*/i.test(family):
+    case /.*roboto.*/i.test(family):
+      hang = "open";
+    break;
+    case /.*nyt-imperial.*/i.test(family):
+    case /.*indy ?serif.*/i.test(family):
+    case /.*georgia.*/i.test(family):
+      hang = "hang-nyt";
+    break;
+    case /.*merriweather.*/i.test(family):
+      hang = "merriweather";
+    break;
+    case /.*brevetext.*/i.test(family):
+      hang = "brevetext";
+    break;
+    default:
+      hang = "open";
+      console.warn("No matching font family found, using default 'open' hang value.");
+  }
+
+  document.documentElement.style.setProperty('--hang', `var(--${hang})`);
+
+  const partialHang = new Set(['A', 'o']);
+
+  paragraphs.forEach( p => p.innerHTML = noBreak(p.innerHTML));
+  paragraphs.forEach( p => {
+                      if (p.innerText[0] === "“") { 
+                        if (partialHang.has(p.innerText[1])) {
+                          p.className = p.className + " single-quote";
+                        } else {
+                          p.className = p.className + " hanging-quote";
+                        }
+                      }
+                  });
+  paragraphs.forEach( p => p.innerHTML = p.innerHTML
+                            .replace(/((\w+[^\w\s]?\s)(?=“\w+))(“\w+)/g, "<span class='before-quote'>$1</span><span class='hanging-quote'>$3</span>")
+                            .replace(/((\w+[^\w\s]?\s)(?=‘\w+))(‘\w+)/g, "<span class='before-single-quote'>$1</span><span class='single-quote'>$3</span>")
+                    );
+
 }
 
-document.documentElement.style.setProperty('--hang', `var(--${hang})`);
-
-paragraphs.forEach( p => p.innerHTML = noBreak(p.innerHTML));
-paragraphs.forEach( p => p.innerText[0] === "“" ? 
-                         p.className = p.className + " hanging-quote" : "");
-paragraphs.forEach( p => p.innerHTML = p.innerHTML
-                          .replace(/((\w+[^\w\s]?\s)(?=“\w+))(“\w+)/g, "<span class='before-quote'>$1</span><span class='hanging-quote'>$3</span>")
-                          .replace(/((\w+[^\w\s]?\s)(?=‘\w+))(‘\w+)/g, "<span class='before-single-quote'>$1</span><span class='single-quote'>$3</span>")
-                  );
+setTimeout(() => {
+  hangQuotes();
+  console.log("Timeout executed, hanging quotes applied!");
+}, 180);
